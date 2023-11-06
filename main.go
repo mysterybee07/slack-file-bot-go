77ace6a1109b16961f9fcc8f243f61a7ebe09763
mysterybee07 <biraj2019pudasaini@gmail.com>
2023-11-06 11:21:54 +0545

    slack-bot created

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5981424515030-6171364659808-54LEaDfPJxM87MS4MKYZCsGR")
+	os.Setenv("CHANNEL_ID", "C05V4F6QXKN")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"adminer.php"}
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name:%s, URL:%s\n", file.Name, file.URL)
+	}
+
+}
